330dd8665e8ebf2ed725f78f1f9bd6054eca4e57
Preyde <60644448+Preyde@users.noreply.github.com>
2020-09-23 19:40:48 +0200

    Add files via upload

--- chap_client/mod.ts
+++ chap_client/mod.ts
@@ -1,9 +1,56 @@
-const data = await fetch("http://localhost:8000")
+import { readLines } from "https://deno.land/std/io/mod.ts"
+import { createHash } from "https://deno.land/std/hash/mod.ts"
+import * as log from "https://deno.land/std/log/mod.ts"
 
-if (data) {
+let challenge = ''
+
+try {
+    const data = await fetch("http://localhost:8000")
     const json = await data.json()
-    console.log(json)
+    challenge = json.challenge.toString()
+} catch (err) {
+    log.error("Server ist offline!")
+    Deno.exit()
 }
+
+
+console.log("Benutzernamen eingeben")
+const userNameData = await readLines(Deno.stdin).next()
+const userName = (userNameData.value as string).replace('\r', '')
+
+console.log("Passwort eingeben")
+const passwordData = await readLines(Deno.stdin).next();
+const password = (passwordData.value as string).replace('\r', '')
+
+
+const hash = createHash("md5")
+
+hash.update(challenge)
+hash.update(password)
+
+const body = {
+    userName: userName,
+    hash: hash.toString()
+}
+
+const response = await fetch("http://localhost:8000", {
+
+    headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+    },
+    body: JSON.stringify(body),
+    method: 'POST'
+})
+
+const { authenticated } = await response.json().catch(() => log.error("Falsche Login-Daten"))
+
+if (authenticated) {
+    log.info("Login erfolgreich")
+} else {
+    log.error("Login fehlgeschlagen")
+}
+
 
 
 
--- chap_server/mod.ts
+++ chap_server/mod.ts
@@ -1,21 +1,56 @@
-import { Application } from "https://deno.land/x/oak@v6.2.0/mod.ts"
+import { Application, Router } from "https://deno.land/x/oak@v6.2.0/mod.ts"
 import { DB } from "https://deno.land/x/sqlite@v2.3.0/mod.ts"
 import { createHash } from "https://deno.land/std/hash/mod.ts"
 
 const app = new Application()
+const router = new Router()
 const PORT = 8000
 
-app.use(ctx => {
-    const challenge = Math.random()
-    const hash = createHash("md5")
-    hash.update(challenge.toString())
-    hash.update("test")
-    ctx.response.body = { hash: hash.toString() }
-})
+
+
+
+
+const challenge = Math.random()
+
+
+
+router
+    .get('/', ctx => {
+        ctx.response.body = {
+            challenge: challenge
+        }
+    })
+    .post('/', async ctx => {
+
+        const request = await ctx.request.body().value
+        // user = [<name>, <passwort>]
+
+        const db = new DB('user.db')
+
+        const query = db.query(`SELECT password FROM user WHERE name = '${request.userName}'`).next()
+
+        const password = query.value[0]
+
+        const hash = createHash("md5")
+        hash.update(challenge.toString())
+        hash.update(password)
+
+        const response = {
+            authenticated: request.hash === hash.toString() ? true : false
+        }
+
+        ctx.response.headers.set('Content-Type', 'application/json')
+        ctx.response.body = JSON.stringify(response)
+    })
+
+
+app.use(router.allowedMethods())
+app.use(router.routes())
+
 
 if (import.meta.main) {
     app.listen({
-        //hostname: '0.0.0.0',
+        hostname: '0.0.0.0',
         port: PORT
     })
 }
